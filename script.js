1550e783dbae8392c4816c39994e76ebf84ef1ad
Mangesh Bodke <mangeshbodke582@gmail.com>
2025-05-02 19:17:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstChar(text) {
-for(let i=0;i<=text.length-1;i++){
-	console.log(text(0));
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] !== ' ') {
+            return text[i];
+        }
+    }
+    return '';
 }
-	
-}
-
-firstChar("mangesh");
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
